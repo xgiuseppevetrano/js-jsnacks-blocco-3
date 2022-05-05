e686de1e7a4bfa7ff085c9f272e999d6dcf72ccc
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-05-05 15:35:05 +0200

    add base form snack-3

---
+++ snack-3/main.js
@@ -0,0 +1,4 @@
+// Crea due tag div con due id diversi:
+// un div avrà il testo colorato di rosso mentre l’altro di verde.
+// Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
+
