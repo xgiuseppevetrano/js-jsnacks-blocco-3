42297f3226f1cd1eb9ed2832b7a88c86e1188141
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-05-05 17:06:06 +0200

    add for cicle to create array and append in html

--- snack-3/main.js
+++ snack-3/main.js
@@ -6,14 +6,25 @@
 const divOne = document.createElement("div");
 const divTwo = document.createElement("div");
 
+const container = document.querySelector(".container");
+container.append(divOne, divTwo);
+
 // 2. Assegno ad ogni div un id diverso
 //    Ogni id corrisponde ad un colore del testo (rosso - verde)
-divOne.id("red");
-divTwo.id("green");
+divOne.id = "red";
+divTwo.id = "green";
 
-// 3. Creo un array con una quantità casuale di numeri tra 1 e 20
+// 3. Creo un array con una quantità casuale di numeri tra 10 e 30
+let number = [];
+for (let i = 0; i < Math.floor(Math.random() * 30) + 10; i++) {
+    // 4. All'interno dell'array metto dei numeri casuali tra 1 e 100
+    number.push(Math.floor(Math.random() * 100) + 1);
 
-// 4. Stampo nel div con id rosso i numeri dispari
-
-// 5. Stampo nel div con id verde i numeri pari
-
+    if (number[i] % 2 === 1) {
+        // 5. Stampo nel div con id rosso i numeri dispari
+        divOne.innerText += ` ${number[i]}`;
+    } else {
+        // 6. Stampo nel div con id verde i numeri pari
+        divTwo.innerText += ` ${number[i]}`;
+    }
+}
