ed34656e96733e70547d7d671e79ba286aafc82f
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-05-05 16:15:33 +0200

    create two div with an id

--- snack-3/main.js
+++ snack-3/main.js
@@ -2,3 +2,18 @@
 // un div avrà il testo colorato di rosso mentre l’altro di verde.
 // Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
 
+// 1. Creo due tag div
+const divOne = document.createElement("div");
+const divTwo = document.createElement("div");
+
+// 2. Assegno ad ogni div un id diverso
+//    Ogni id corrisponde ad un colore del testo (rosso - verde)
+divOne.id("red");
+divTwo.id("green");
+
+// 3. Creo un array con una quantità casuale di numeri tra 1 e 20
+
+// 4. Stampo nel div con id rosso i numeri dispari
+
+// 5. Stampo nel div con id verde i numeri pari
+
