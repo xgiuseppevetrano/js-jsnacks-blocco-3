874f2f60d7d64d55f8c44b3c08527430f09ee567
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-05-05 15:20:03 +0200

    add prompt

---
+++ snack-2/main.js
@@ -0,0 +1,10 @@
+// Fai inserire un numero, che chiameremo N, all’utente.
+// Genera N array, ognuno formato da 10 numeri casuali tra 1 e 100. Ogni volta che ne crei uno, stampalo a schermo.
+
+// 1. Chiedo all'utente un numero che chiamerò N
+const N = Number(prompt("Inserisci un numero"));
+
+// 2. Creo tanti array quanto è N
+//    Ogni array è formato da 10 numeri casuali tra 1 e 100
+
+// 3. Ogni array creato viene stampato in console
